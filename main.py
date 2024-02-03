154fb7d07343578c61e070e456b5fd2c3f39a00e
Ellie Burton <116921993+ellie-burton@users.noreply.github.com>
2024-02-03 14:07:51 -0600

    edit game caption

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import sys
 def Main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption('My Game')
+    pygame.display.set_caption('Nebula Navigators')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.fill((0, 0, 0))
+        screen.fill((255, 255, 255))
         pygame.display.flip()
 
 if __name__ == '__main__':
