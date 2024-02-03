e637d14fa19926306e03e74ec02e63301938bd1f
Ellie Burton <116921993+ellie-burton@users.noreply.github.com>
2024-02-03 14:14:15 -0600

    rocket icon

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def Main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Nebula Navigators')
+    pygame_icon = pygame.image.load('rocket.svg')
+    pygame.display.set_icon(pygame_icon)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
