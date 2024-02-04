cc404d62b020d30d18744b23435dcd827a7bb1a4
JackMincey <jwmincey@crimson.ua.edu>
2024-02-04 01:16:31 -0600

    camera tweaks

--- camera.py
+++ camera.py
@@ -6,18 +6,18 @@ class Camera(pygame.sprite.Group):
         self.display_surface = pygame.display.get_surface()
 
         # starting point for background
-        self.px = 0
-        self.py = 0
+        self.px = -4800
+        self.py = -3240
 
         # space
         image = pygame.image.load('Stars.jpg')
         image = pygame.transform.scale(image, (1920, 1080))
         self.space_surface = image.convert_alpha()
-        self.space=pygame.Surface((1920*4,1080*4), pygame.SRCALPHA)
+        self.space=pygame.Surface((1920*5,1080*6), pygame.SRCALPHA)
         i=0
-        while(i<4):
+        while(i<5):
             j=0
-            while(j<4):
+            while(j<6):
                 self.space.blit(self.space_surface,(i*1920,j*1080))
                 j+=1
             i+=1
